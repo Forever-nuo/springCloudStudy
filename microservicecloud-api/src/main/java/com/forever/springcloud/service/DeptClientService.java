79c1f523504a627829f81270b9ae0096ea8e44be
forever <857173891@qq.com>
2018-07-25 00:43:47 +0800

    feign的完成

---
+++ microservicecloud-api/src/main/java/com/forever/springcloud/service/DeptClientService.java
@@ -0,0 +1,23 @@
+package com.forever.springcloud.service;
+
+import com.forever.springcloud.dto.ResponseData;
+import com.forever.springcloud.entity.Dept;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author: Forever丶诺
+ * @createTime: 2018-7-24.23:30
+ */
+@FeignClient("MICROSERVICECLOUD-DEPT")
+@RequestMapping("/dept")
+public interface DeptClientService {
+
+    @GetMapping("/{id}")
+    ResponseData<Dept> getDeptById(@PathVariable("id") Integer id);
+
+    @GetMapping("list")
+    ResponseData<Dept> listDept();
+}
