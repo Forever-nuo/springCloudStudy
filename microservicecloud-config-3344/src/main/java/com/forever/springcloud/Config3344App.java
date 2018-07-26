a95d2d4d50483ae0a6f038edb483a8a92c1f0fed
forever <857173891@qq.com>
2018-07-27 00:45:02 +0800

    config的初始演示

--- microservicecloud-config-3344/src/main/java/com/forever/springcloud/Config3344App.java
+++ microservicecloud-config-3344/src/main/java/com/forever/springcloud/Config3344App.java
@@ -2,17 +2,17 @@ package com.forever.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @author: Forever丶诺
- * @createTime: 2018-7-26.21:38
+ * @createTime: 2018-7-26.23:53
  */
 @SpringBootApplication
-@EnableZuulProxy
-public class Zuul9527App {
+@EnableConfigServer
+public class Config3344App {
 
     public static void main(String[] args) {
-        SpringApplication.run(Zuul9527App.class, args);
+        SpringApplication.run(Config3344App.class, args);
     }
 }
